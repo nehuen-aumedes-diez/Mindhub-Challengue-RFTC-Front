0c6f803eff92ce3a5273ff38e60c66abb9905001
ignaarguello <igna.arguello2110@hotmail.com>
2022-12-13 13:15:44 -0300

    more commits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Carousel from './components/Carousel/Carousel';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Carousel/>
+      
     </div>
   );
 }
