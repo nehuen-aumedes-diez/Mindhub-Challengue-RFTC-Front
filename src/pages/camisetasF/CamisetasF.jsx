0832884b660cf37be1be53f8e409fd39990ac51b
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-12-14 19:50:05 -0300

    SignIn-Up2

--- src/pages/camisetasF/CamisetasF.jsx
+++ src/pages/camisetasF/CamisetasF.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CardRemera from '../../components/CardRemera/CardRemera'
+import CardRemera from '../../components/cardRemera/CardRemera'
 
 function CamisetasF() {
   return (
