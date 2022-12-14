30d718f67d631740c087406aa8d71c323644be5c
ignaarguello <igna.arguello2110@hotmail.com>
2022-12-14 17:02:46 -0300

    Componente slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import CamisetasF from "./pages/camisetasF/CamisetasF";
 import CamisetasFDetalles from "./pages/CamisetasFDetalles/CamisetasFDetalles";
 import Probando from "./components/Probando";
