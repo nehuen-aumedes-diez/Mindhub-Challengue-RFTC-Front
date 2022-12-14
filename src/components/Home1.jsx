0485bf9c7a4af73ce52c4ce2de05cfb91b7cbc52
ignaarguello <igna.arguello2110@hotmail.com>
2022-12-14 00:57:57 -0300

    Component Cards Home

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -3,51 +3,24 @@ import '../styles/home1.css';
 
 
 export default function Home1() {
-  const tittle = "¡CADA VEZ MÁS CERCA!.";
-  const sub = " Pintemos la semifinal de celeste y blanco. ¡Todos con Argentina! "
-return(
-<>
-
-<div className='banner1'>
-<div className='logo'>
-    <img className='logo1' src=''></img>
-</div>
-<img className='banner' src='https://cnnespanol.cnn.com/wp-content/uploads/2022/06/GettyImages-1400968249-e1654535112446.jpg?quality=100&strip=info' ></img>
-<h1 className="tittle-home">{tittle}</h1>
-<p className='subt'>{sub}</p>
-
-
-
-<div className='btn1-cont'>
-
-<button className='btn1'> ver camiseta </button>
-
-</div>
-
-<div className='btn2-cont'>
-
-<button className='btn2'> conocé más </button>
-
-</div>
-
-</div>
-
- <div>
-
-    <img className='imgCamisetas' src="//cdn.shopify.com/s/files/1/0016/1061/2806/files/20221101_153113.jpg?v=1667458746&width=2200 2200w"></img>
-
-</div> 
-
-
-
-
-
-</>
-
-
-
-
-
-
-)
+  const tittle = "¡CADA VEZ MÁS CERCA!";
+  const sub = " Pintemos la semifinal de celeste y blanco ¡Todos con Argentina! "
+  return (
+    <>
+      <div className='banner1'>
+        <div className='logo'>
+          <img className='logo1' src=''></img>
+        </div>
+        <img className='banner' src='https://cnnespanol.cnn.com/wp-content/uploads/2022/06/GettyImages-1400968249-e1654535112446.jpg?quality=100&strip=info' ></img>
+        <h1 className="tittle-home">{tittle}</h1>
+        <p className='subt'>{sub}</p>
+        <div className='btn1-cont'>
+          <button className='btn1'> ver camiseta </button>
+        </div>
+        <div className='btn2-cont'>
+          <button className='btn2'> conocé más </button>
+        </div>
+      </div>
+    </>
+  )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Home1 from "../components/Home1"
 import Navbar from "../components/Navbar/Navbar"
+import CardHome from '../components/CardHome/CardHome'
+
 
 
 export default function Home() {
   return (
     <>
     <Navbar />
-    <Home1/>
+    {/* <Home1/> */}
+    <CardHome/>
     </>
   )
 }
