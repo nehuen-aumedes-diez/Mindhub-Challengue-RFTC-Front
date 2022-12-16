7f2358113edc2bde57e666e80dbec0a0d1dc59ad
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2022-12-16 03:12:40 -0300

    pull develop2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import CamisetasF from "./pages/camisetasF/CamisetasF";
+import CamisetasF from "./pages/CamisetasF/CamisetasF";
 import Home from "./pages/Home/Home";
 import CamisetasFDetalles from "./pages/CamisetasFDetalles/CamisetasFDetalles";
 import { Routes, Route } from "react-router-dom";
@@ -14,7 +14,7 @@ import CamisetasMDetalles from "./pages/CamisetasMDetalles/CamisetasMDetalles";
 import BuzosDetalles from "./pages/BuzosDetalles/BuzosDetalles";
 import GorrasDetalles from "./pages/GorrasDetalles/GorrasDetalles";
 import Contacto from "./pages/contacto/Contacto";
-import Nosotros from './pages/nosotros/Nosotros'
+import Nosotros from './pages/Nosotros/Nosotros'
 import Contador from "./components/Contador/Contador";
 import PaginaDeStock from './pages/PaginaDeStock/PaginaDeStock'
 
--- src/pages/Gorras/Gorras.jsx
+++ src/pages/Gorras/Gorras.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import gorraActions from '../../redux/actions/gorraAction';
-import CardRemera from "../../components/cardRemera/CardRemera"
+import CardRemera from "../../components/CardRemera/CardRemera"
 import Market from '../../components/market/Market'
 import SearchBar from '../../components/searchBar/SearchBar'
 import Select from '../../components/order/Select'
