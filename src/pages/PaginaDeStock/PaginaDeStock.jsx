28b67306f4a8a2ad4645673e37ed714eb3daafbb
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-12-21 10:14:51 -0300

    cositas

--- src/pages/PaginaDeStock/PaginaDeStock.jsx
+++ src/pages/PaginaDeStock/PaginaDeStock.jsx
@@ -1,5 +1,5 @@
 import './PaginaDeStock.css'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import remeraFActions from '../../redux/actions/remeraFAction'
 import remeraMActions from '../../redux/actions/remeraMAction';
@@ -28,11 +28,48 @@ export default function PaginaDeStock() {
   }, [])
 
   let remeritaF = remerasF[0]?.foto1
-  let remeritaM = remerasM[0]?.foto1
-  let gorrita = gorras[0]?.foto1
-  let bucito = buzos[0]?.foto1
+  let cantidadF = remerasF.length
+  let contadorF = [0]
+  for (var i = 0; i < remerasF.length; i++) {
+    contadorF.push(remerasF[i].stock.reduce((viejo, nuevo) => viejo + nuevo));
+  }
+  let todasF = contadorF.reduce((viejo, nuevo) => viejo + nuevo)
+  let tallesF = []
+  for (var e = 0; e < remerasF.length; e++) {
+    console.log(remerasF[e].talle);
+    while (remerasF[e].talle>3) {
+      tallesF.push(remerasF[e].talle)
+    }
+  }
+  console.log(tallesF);
 
-  console.log(remeritaF);
+  let remeritaM = remerasM[0]?.foto1
+  let cantidadM = remerasM.length
+  let contadorM = [0]
+  for (var a = 0; a < remerasM.length; a++) {
+    contadorM.push(remerasM[a].stock.reduce((viejo, nuevo) => viejo + nuevo));
+  }
+  let todasM = contadorM.reduce((viejo, nuevo) => viejo + nuevo)
+
+  let gorrita = gorras[0]?.foto1
+  let cantidadG = gorras.length
+  let contadorG = [0]
+  for (var b = 0; b < gorras.length; b++) {
+    contadorG.push(gorras[b].stock);
+  }
+  let todasG = contadorG.reduce((viejo, nuevo) => viejo + nuevo)
+
+  let bucito = buzos[0]?.foto1
+  let cantidadB = buzos.length
+  let contadorB = [0]
+  for (var c = 0; c < buzos.length; c++) {
+    contadorB.push(buzos[c].stock.reduce((viejo, nuevo) => viejo + nuevo));
+  }
+  let todasB = contadorB.reduce((viejo, nuevo) => viejo + nuevo)
+
+
+
+
   return (
     <div>
 
@@ -42,43 +79,30 @@ export default function PaginaDeStock() {
             <img className='imagenStock' src={`${remeritaF}`} alt="foto de remera de mujer " />
           </div>
           <div class="more-info">
-            <h1></h1>
-            <div class="coords">
-              <span>Remeras Femeninas</span>
-              <span>Joined January 2019</span>
-            </div>
-            <div class="coords">
-              <span>Position/Role</span>
-              <span>City, Country</span>
-            </div>
+            <h1>Remeras Femeninas</h1>
             <div class="stats">
               <div>
-                <div class="title">Awards</div>
+                <div class="title">Modelos</div>
                 <i class="fa fa-trophy"></i>
-                <div class="value">2</div>
+                <div class="value">{`${cantidadF}`}</div>
               </div>
               <div>
-                <div class="title">Matches</div>
+                <div class="title">Unidades</div>
                 <i class="fa fa-gamepad"></i>
-                <div class="value">27</div>
+                <div class="value">{`${todasF}`}</div>
               </div>
               <div>
-                <div class="title">Pals</div>
+                <div class="title">Talles</div>
                 <i class="fa fa-group"></i>
-                <div class="value">123</div>
+                <div class="value">{`${tallesF}`}</div>
               </div>
-              <div>
-                <div class="title">Coffee</div>
-                <i class="fa fa-coffee"></i>
-                <div class="value infinity">∞</div>
-              </div>
+              <div class="value infinity"><img className='imgMas' src="./icons8-more-67.png" alt="mas" /></div>
             </div>
           </div>
         </div>
         <div class="generalA">
-          <h1>Jane Doe</h1>
+          <h1>Remeras Femeninas</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a volutpat mauris, at molestie lacus. Nam vestibulum sodales odio ut pulvinar.</p>
-          <span class="more">Mouse over the card for more info</span>
         </div>
       </div>
 
@@ -88,43 +112,30 @@ export default function PaginaDeStock() {
             <img className='imagenStock' src={`${remeritaM}`} alt="foto de remera de mujer " />
           </div>
           <div class="more-info">
-            <h1>Jane Doe</h1>
-            <div class="coords">
-              <span>Group Name</span>
-              <span>Joined January 2019</span>
-            </div>
-            <div class="coords">
-              <span>Position/Role</span>
-              <span>City, Country</span>
-            </div>
+            <h1>Remeras Masculinas</h1>
             <div class="stats">
               <div>
-                <div class="title">Awards</div>
+                <div class="title">Modelos</div>
                 <i class="fa fa-trophy"></i>
-                <div class="value">2</div>
+                <div class="value">{`${cantidadM}`}</div>
               </div>
               <div>
-                <div class="title">Matches</div>
+                <div class="title">Unidades</div>
                 <i class="fa fa-gamepad"></i>
-                <div class="value">27</div>
+                <div class="value">{`${todasM}`}</div>
               </div>
               <div>
-                <div class="title">Pals</div>
+                <div class="title">Talles</div>
                 <i class="fa fa-group"></i>
                 <div class="value">123</div>
               </div>
-              <div>
-                <div class="title">Coffee</div>
-                <i class="fa fa-coffee"></i>
-                <div class="value infinity">∞</div>
-              </div>
+              <div class="value infinity"><img className='imgMas' src="./icons8-more-67.png" alt="mas" /></div>
             </div>
           </div>
         </div>
         <div class="generalA">
-          <h1>Jane Doe</h1>
+          <h1>Remeras Masculinas</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a volutpat mauris, at molestie lacus. Nam vestibulum sodales odio ut pulvinar.</p>
-          <span class="more">Mouse over the card for more info</span>
         </div>
       </div>
 
@@ -134,43 +145,30 @@ export default function PaginaDeStock() {
             <img className='imagenStock' src={`${gorrita}`} alt="foto de remera de mujer " />
           </div>
           <div class="more-info">
-            <h1>Jane Doe</h1>
-            <div class="coords">
-              <span>Group Name</span>
-              <span>Joined January 2019</span>
-            </div>
-            <div class="coords">
-              <span>Position/Role</span>
-              <span>City, Country</span>
-            </div>
+            <h1>Gorras</h1>
             <div class="stats">
               <div>
-                <div class="title">Awards</div>
+                <div class="title">Modelos</div>
                 <i class="fa fa-trophy"></i>
-                <div class="value">2</div>
+                <div class="value">{`${cantidadG}`}</div>
               </div>
               <div>
-                <div class="title">Matches</div>
+                <div class="title">Unidades</div>
                 <i class="fa fa-gamepad"></i>
-                <div class="value">27</div>
+                <div class="value">{`${todasG}`}</div>
               </div>
               <div>
-                <div class="title">Pals</div>
+                <div class="title">Talles</div>
                 <i class="fa fa-group"></i>
                 <div class="value">123</div>
               </div>
-              <div>
-                <div class="title">Coffee</div>
-                <i class="fa fa-coffee"></i>
-                <div class="value infinity">∞</div>
-              </div>
+              <div class="value infinity"><img className='imgMas' src="./icons8-more-67.png" alt="mas" /></div>
             </div>
           </div>
         </div>
         <div class="generalA">
-          <h1>Jane Doe</h1>
+          <h1>Gorras</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a volutpat mauris, at molestie lacus. Nam vestibulum sodales odio ut pulvinar.</p>
-          <span class="more">Mouse over the card for more info</span>
         </div>
       </div>
 
@@ -180,43 +178,30 @@ export default function PaginaDeStock() {
             <img className='imagenStock' src={`${bucito}`} alt="foto de remera de mujer " />
           </div>
           <div class="more-info">
-            <h1>Jane Doe</h1>
-            <div class="coords">
-              <span>Group Name</span>
-              <span>Joined January 2019</span>
-            </div>
-            <div class="coords">
-              <span>Position/Role</span>
-              <span>City, Country</span>
-            </div>
+            <h1>Buzos</h1>
             <div class="stats">
               <div>
-                <div class="title">Awards</div>
+                <div class="title">Modelos</div>
                 <i class="fa fa-trophy"></i>
-                <div class="value">2</div>
+                <div class="value">{`${cantidadB}`}</div>
               </div>
               <div>
                 <div class="title">Matches</div>
                 <i class="fa fa-gamepad"></i>
-                <div class="value">27</div>
+                <div class="value">{`${todasB}`}</div>
               </div>
               <div>
-                <div class="title">Pals</div>
+                <div class="title">Talles</div>
                 <i class="fa fa-group"></i>
                 <div class="value">123</div>
               </div>
-              <div>
-                <div class="title">Coffee</div>
-                <i class="fa fa-coffee"></i>
-                <div class="value infinity">∞</div>
-              </div>
+              <div class="value infinity"><img className='imgMas' src="./icons8-more-67.png" alt="mas" /></div>
             </div>
           </div>
         </div>
         <div class="generalA">
-          <h1>Jane Doe</h1>
+          <h1>Buzos</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a volutpat mauris, at molestie lacus. Nam vestibulum sodales odio ut pulvinar.</p>
-          <span class="more">Mouse over the card for more info</span>
         </div>
       </div>
 
